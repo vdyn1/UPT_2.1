a407cfb8d30f1dca2ed389a41cfb484b917b4833
v.d.o_ <dimavo2005@gmail.com>
2024-10-07 10:46:38 +0200

    Specified who adds

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,13 @@
-import javax.security.auth.Subject;
-
 
 // TODO: we need to add the missing classes!
+// OK, I will add `Adder` and s30176 will add `Subtractor`.
 public class Main {
     public static void main(String[] args) {
 
-        Adder adder = new Adeer();
-        System.out.println(adder.add(1,2));
+        Adder adder = new Adder();
+        System.out.println(adder.add(1, 2));
+        Subtractor subtractor = new Subtractor();
+        System.out.println(subtractor.subtract(6, 3));
 
-        Substractor substractor = new Substactor();
-        System.out.println(substractor.substract(6,3));
     }
-
 }
