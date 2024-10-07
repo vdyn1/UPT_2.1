db490cd7e6c6fbfb39cde83026ea478ba5939168
v.d.o_ <dimavo2005@gmail.com>
2024-10-07 10:34:58 +0200

    init commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+import javax.security.auth.Subject;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Adder adder = new Adeer();
+        System.out.println(adder.add(1,2));
+
+        Substractor substractor = new Substactor();
+        System.out.println(substractor.substract(6,3));
+    }
+
+}
